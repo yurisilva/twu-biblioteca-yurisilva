7b11562cda6a497e591712561cf753708f6bd5ab
ydonasc <yurinascimentofarias@gmail.com>
2020-01-23 18:22:14 -0300

    Tests and code for the cards 1.1 and 1.2 from Biblioteca Assignment.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,24 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+    public String welcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public ArrayList<String> getAllBooks() {
+        return new ArrayList<String>(){
+            {
+                add("Dorian Grey");
+                add("Outliers");
+                add("Pride and Prejudice");
+            }
+        };
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -9,5 +13,31 @@ public class ExampleTest {
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void viewWelcomeMessageTest(){
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        String actualMessage = bibliotecaApp.welcomeMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void viewAllBooks(){
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ArrayList<String> allBooks = bibliotecaApp.getAllBooks();
+        ArrayList<String> allExpectedBooks = new ArrayList<String>(){
+            {
+                add("Dorian Grey");
+                add("Outliers");
+                add("Pride and Prejudice");
+            }
+        };
+
+        for (int i = 0; i < allBooks.size(); i++) {
+            assertEquals(allBooks.get(i), allExpectedBooks.get(i));
+        }
     }
 }
