f765a4e787c7ea5e45668d87c1316255b18e51c6
layuri.silva <yuri.silva@thoughtworks.com>
2020-02-11 20:56:19 -0300

    refactoring

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,43 +1,72 @@
 package com.twu.biblioteca;
 
-import com.twu.beans.Book;
 import com.twu.repository.Catalog;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BibliotecaApp {
 
-    private Catalog catalog = new Catalog();
+    private static Catalog catalog = new Catalog();
 
     public static void main(String[] args) {
         System.out.println("twu-biblioteca-yurisilva");
         System.out.println(welcomeMessage());
         System.out.println(displayMainMenu());
+
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+
+        while (!userChoice.equals("q")){
+            String result = menuChoice(userChoice);
+
+            System.out.println(result);
+            System.out.println(displayMainMenu());
+
+            userChoice = scanner.nextLine();
+        }
     }
 
     public static String welcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
 
-    public String invalidOptionSelected() {
+    public static String invalidOptionSelected() {
         return "Please select a valid option!";
     }
 
     public static String displayMainMenu() {
-        return "Select an option:\n1 - List of books\nq - Quit";
+        return "Select an option:\n1 - List of books\n2 - List of Books (detailed)\n3 - Checkout a book\n4 - Return a book\nq - Quit";
     }
 
-    public String menuChoice(String userInput) {
+    public static String menuChoice(String userInput) {
         switch (userInput){
             case "1":
                 return catalog.getAllBooks();
+            case "2":
+                return catalog.getAllBooksVerbose();
+            case "3":
+                showCheckoutBookMenu();
+            case "4":
+                showReturnBookMenu();
             case "q":
                 return "Bye!";
             default:
                 return invalidOptionSelected();
         }
+    }
+
+    private static void showCheckoutBookMenu() {
+        System.out.println("Please provide book ID");
+        Scanner consoleInput = new Scanner(System.in);
+        String bookId = consoleInput.nextLine();
+        System.out.println(catalog.checkout(bookId));
+    }
+
+    private static void showReturnBookMenu() {
+        System.out.println("Please provide book ID");
+        Scanner consoleInput = new Scanner(System.in);
+        String bookId = consoleInput.nextLine();
+        System.out.println(catalog.returnBook(bookId));
     }
 
     public String checkoutBookId(String bookIdToCheckout) {
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -4,9 +4,6 @@ import com.twu.repository.Catalog;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
